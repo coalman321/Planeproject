6cda6edb52b8d74c6416c65481a37ab7583dd40e
coalman321 <coalman321@users.noreply.github.com>
2016-02-22 13:39:48 -0500

    Update GpsForPlane.h
    removed sofwareserial inclusion

--- MegaCode/GpsForPlane.h
+++ MegaCode/GpsForPlane.h
@@ -1,15 +1,14 @@
 #include <TinyGPS.h>
+#include "Arduino.h"
 
 /*
     GpsForPlane.h - Library for processing GPS
     Created by Joy Smith and Cole Tucker Feb. 2016
     Welcome to the Danger Zone, proceed with caution
  */
-#ifndef GpsForPlane_h
-#define GpsForPlane_h
 
-#include "Arduino.h"
-#include <SoftwareSerial.h>
+
+
 TinyGPS gps;
 
 class GpsForPlane : public TinyGPS
